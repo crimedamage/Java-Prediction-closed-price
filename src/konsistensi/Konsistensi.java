6e8cdc7ef7c651df0b95ce0228ae519ad7914e51
crimedamage <blackhuman589@gmail.com>
2020-04-26 17:31:52 +0700

    test

---
+++ src/konsistensi/Konsistensi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package konsistensi;
+
+/**
+ *
+ * @author user
+ */
+public class Konsistensi {
+
+    /**
+     * @param args the command line arguments   
+     */
+    public  static void main(String[] args) {
+        // TODO code application logic here
+        log();
+    }
+    private static void log(){
+        login_form lf=new login_form();
+        lf.setVisible(true);
+    }
+    
+    
+}   
